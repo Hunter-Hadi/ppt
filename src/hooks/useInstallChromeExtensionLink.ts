f270e55dd983a6c177b2c145eac11d25118656ad
zhouru1bin <ruibin@simplylab.xyz>
2023-06-12 10:18:15 +0800

    fix: install link

--- src/hooks/useInstallChromeExtensionLink.ts
+++ src/hooks/useInstallChromeExtensionLink.ts
@@ -1,11 +1,13 @@
 import { useRouter } from 'next/router';
 import { useMemo } from 'react';
 
+import { INSTALL_LINK } from '@/global_constants';
+
 const DEFAULT_APP_HEADER_TOPBAR = 'topbar';
 
 const useInstallChromeExtensionLink = (isTopbar = false) => {
   const { query, pathname, isReady } = useRouter();
-  const prefix = 'https://api.maxai.me/app';
+  const prefix = INSTALL_LINK;
   const installChromeExtensionLink = useMemo(() => {
     let defaultRef =
       (isTopbar ? DEFAULT_APP_HEADER_TOPBAR : '') +
@@ -16,13 +18,13 @@ const useInstallChromeExtensionLink = (isTopbar = false) => {
     }
 
     if (pathname === '/partner-referral') {
-      return `${prefix}/install?ref=${query.ref || defaultRef}`;
+      return `${prefix}?ref=${query.ref || defaultRef}`;
     } else if (pathname.startsWith('/prompts')) {
-      return `${prefix}/install?ref=${query.ref || defaultRef}`;
+      return `${prefix}?ref=${query.ref || defaultRef}`;
     } else {
-      return `${prefix}/install?ref=${defaultRef}`;
+      return `${prefix}?ref=${defaultRef}`;
     }
-  }, [query.ref, pathname, isTopbar]);
+  }, [query.ref, pathname, isTopbar, prefix]);
   return {
     installChromeExtensionLink,
     loading: !isReady,
