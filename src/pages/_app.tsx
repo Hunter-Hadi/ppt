5f983d59ada268a6c31ee3e58117947d99350132
zhouru1bin <ruibin@simplylab.xyz>
2023-06-27 16:00:26 +0800

    fix: show Announcement

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,6 +14,7 @@ import { RecoilRoot } from 'recoil';
 
 import AppFooter from '@/app_layout/AppFooter';
 import AppHeader from '@/app_layout/AppHeader';
+import Announcement from '@/components/Announcement';
 import customMuiTheme from '@/config/customMuiTheme';
 import globalFont from '@/config/font';
 import { GA_TRACKING_ID } from '@/pages/_document';
@@ -143,7 +144,7 @@ export default function App({ Component, pageProps }: AppProps) {
             <QueryClientProvider client={queryClient}>
               <ReactQueryDevtools initialIsOpen={false} />
               <AppInit />
-              {/* <Announcement /> */}
+              <Announcement />
               <AppHeader />
               <Component {...pageProps} />
               <AppFooter />
