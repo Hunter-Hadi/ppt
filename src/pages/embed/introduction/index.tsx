a8278f8ee532d262b7f2fd7883f12124ff59a614
zhouru1bin <ruibin@simplylab.xyz>
2023-06-29 11:07:35 +0800

    chore: EmbedIntroduction

--- src/pages/embed/introduction/index.tsx
+++ src/pages/embed/introduction/index.tsx
@@ -1,7 +1,28 @@
+import { Box } from '@mui/material';
+import { useEffect, useRef } from 'react';
+
 import { HomePageContent } from '@/pages';
 
 const EmbedIntroduction = () => {
-  return <HomePageContent />;
+  const contentRef = useRef<any>(null);
+
+  useEffect(() => {
+    if (window.parent) {
+      window.parent.postMessage(
+        {
+          type: 'embed',
+          height: contentRef.current?.offsetHeight,
+        },
+        '*',
+      );
+    }
+  }, []);
+
+  return (
+    <Box ref={contentRef}>
+      <HomePageContent />
+    </Box>
+  );
 };
 
 export default EmbedIntroduction;
