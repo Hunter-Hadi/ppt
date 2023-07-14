e8b4eca4621a6fd8b80e1571f3f96ffe8ea9079b
zhouru1bin <ruibin@simplylab.xyz>
2023-07-14 19:31:37 +0800

    chore: usechatgpt

---
+++ src/pages/usechatgpt.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+
+import AppContainer from '@/app_layout/AppContainer';
+import AppDefaultSeoLayout from '@/app_layout/AppDefaultSeoLayout';
+import HomePageContent from '@/page_components/landing_page/HomePageContent';
+
+const ChromeExtensionPage = () => {
+  useEffect(() => {
+    const hash = window.location.hash;
+    if (hash) {
+      const id = hash.replace('#', '');
+      const element = document.getElementById(id);
+      if (element) {
+        element.scrollIntoView();
+      }
+    }
+  }, []);
+  return (
+    <AppContainer sx={{ bgcolor: '#fff' }}>
+      <AppDefaultSeoLayout />
+
+      <AppDefaultSeoLayout
+        title={'UseChatGPT.AI: Use AI Anywhere (ChatGPT, Bard, Bing, Claude)'}
+        description={
+          'The fastest way to use AI anywhere online. One click to compose, improve writing, summarize, explain, fix spelling & grammar, change tone, translate, or reply to any text.'
+        }
+        canonical={'https://maxai.me/install'}
+      />
+      <HomePageContent showLogo={false} />
+    </AppContainer>
+  );
+};
+export default ChromeExtensionPage;
