2afd6943c2501b9c9fd0b67e5760da48396fc1f0
zhouru1bin <ruibin@simplylab.xyz>
2023-07-19 15:46:56 +0800

    chore: note

--- src/pages/embed/overview/index.tsx
+++ src/pages/embed/overview/index.tsx
@@ -5,7 +5,8 @@ import AppDefaultSeoLayout from '@/app_layout/AppDefaultSeoLayout';
 import useEffectOnce from '@/hooks/useEffectOnce';
 import HomePageContent from '@/page_components/LandingPage/HomePageContent';
 
-const EmbedIntroduction = () => {
+// 用于外部嵌入的 overview 页面
+const EmbedOverview = () => {
   const [loaded, setLoaded] = useState(false);
   const contentRef = useRef<any>(null);
 
@@ -37,4 +38,4 @@ const EmbedIntroduction = () => {
   );
 };
 
-export default EmbedIntroduction;
+export default EmbedOverview;
