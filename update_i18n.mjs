0395949f0704b4e1b5788ef3ead078ed3326590b
zhouru1bin <ruibin@simplylab.xyz>
2024-01-09 21:28:18 +0800

    fix: height

--- update_i18n.mjs
+++ update_i18n.mjs
@@ -489,7 +489,7 @@ async function updateKeys(keys, forceUpdate, retryLanguageCodes = []) {
 }
 
 async function main() {
-  const keys = ['home_page__hero_section__desc'];
+  const keys = [''];
   const retryLanguageCodes = [];
   await updateKeys(keys, keys.length > 0, retryLanguageCodes);
 }
