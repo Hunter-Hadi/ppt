c7b2e441297c3a97ebe4dc1419f68d85a55279a1
zhouru1bin <ruibin@simplylab.xyz>
2024-02-22 10:50:20 +0800

    chore: installed-youtube-video-skip-ad-trigger

---
+++ src/pages/partners/installed/youtube-video-skip-ad-trigger.tsx
@@ -0,0 +1,77 @@
+import { Box, Stack, Typography } from '@mui/material';
+import React from 'react';
+
+import AppDefaultSeoLayout from '@/app_layout/AppDefaultSeoLayout';
+import HomePageContent from '@/features/landing/components/HomePageContent';
+import FixedCtaButton from '@/page_components/partners/FixedCtaButton';
+
+const AIPromptGenius = () => {
+  const propRef = 'installed-youtube-video-skip-ad-trigger';
+  return (
+    <Box
+      sx={{
+        bgcolor: '#fff',
+        position: 'relative',
+        pb: 10,
+      }}
+    >
+      <AppDefaultSeoLayout />
+      <Stack
+        height={336}
+        bgcolor={'#F8F9FA'}
+        alignItems={'center'}
+        justifyContent={'center'}
+        spacing={2}
+        px={{
+          xs: 2,
+          sm: 0,
+        }}
+      >
+        <Typography
+          component='h2'
+          variant='custom'
+          textAlign={'center'}
+          fontSize={48}
+        >
+          🎉 Youtube Video Skip Ad Trigger
+          <br />
+          has been installed
+        </Typography>
+        {/* <Typography>
+          To get started,{' '}
+          <ProLink
+            muiLinkProps={{
+              rel: 'noopener nofollow',
+            }}
+            href='https://docs.aipromptgenius.app/tutorial/01-create-edit-and-use-prompts/'
+            underline='always'
+            target={'_blank'}
+          >
+            click here
+          </ProLink>{' '}
+          to view the first part of the tutorial.
+        </Typography> */}
+      </Stack>
+      <Box position='relative'>
+        <Box
+          sx={{
+            position: 'absolute',
+            top: -24,
+            left: '50%',
+            bgcolor: '#F4EBFB',
+            transform: 'translateX(-50%)',
+            borderRadius: 99,
+            px: 2,
+            py: 1,
+            whiteSpace: 'nowrap',
+          }}
+        >
+          <Typography color='primary'>{`Try our partner's new extension 👇`}</Typography>
+        </Box>
+        <HomePageContent propRef={propRef} />
+      </Box>
+      <FixedCtaButton propRef={propRef} />
+    </Box>
+  );
+};
+export default AIPromptGenius;
