e971e5cc81c5a3b6e2df31ac7fbcc6ce0221d145
zhouru1bin <109497098+zhouru1bin@users.noreply.github.com>
2024-03-27 20:47:41 +0800

    chore: remove test

--- src/pages/[locale]/test-prompts/index.tsx
+++
@@ -1,7 +0,0 @@
-import { makeStaticPaths, makeStaticProps } from '@/i18n/utils/staticHelper';
-import PromptsPages from '@/page_components/PromptPages';
-export default PromptsPages;
-
-const getStaticProps = makeStaticProps();
-const getStaticPaths = makeStaticPaths();
-export { getStaticPaths, getStaticProps };
--- src/pages/test-prompts/index.tsx
+++
@@ -1,6 +0,0 @@
-import { makeStaticProps } from '@/i18n/utils/staticHelper';
-import PromptsPages from '@/page_components/PromptPages';
-export default PromptsPages;
-
-const getStaticProps = makeStaticProps();
-export { getStaticProps };
