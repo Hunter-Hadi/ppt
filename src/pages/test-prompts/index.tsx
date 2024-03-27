37a85fc680547cc5f6a2c164d4fb733dc7868f1c
zhouru1bin <109497098+zhouru1bin@users.noreply.github.com>
2024-03-27 19:59:45 +0800

    test: file

---
+++ src/pages/test-prompts/index.tsx
@@ -0,0 +1,6 @@
+import { makeStaticProps } from '@/i18n/utils/staticHelper';
+import PromptsPages from '@/page_components/PromptPages';
+export default PromptsPages;
+
+const getStaticProps = makeStaticProps();
+export { getStaticProps };
