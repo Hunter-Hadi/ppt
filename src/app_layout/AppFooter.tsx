98113c1aebcaae07f8bfa5a35fb1a46ebfc0f7df
zhouru1bin <109497098+zhouru1bin@users.noreply.github.com>
2024-03-27 15:38:53 +0800

    fix: footer link

--- src/app_layout/AppFooter.tsx
+++ src/app_layout/AppFooter.tsx
@@ -8,7 +8,11 @@ import { useMemo } from 'react';
 import FooterList from '@/components/Footerlist';
 import ProLink from '@/components/ProLink';
 import A16zTop50AppsBadge from '@/features/landing/components/HeroSection/A16zTop50AppsBadge';
-import { APP_EXTERNAL_LINKS, APP_PROJECT_LINK } from '@/global_constants';
+import {
+  APP_EXTERNAL_LINKS,
+  APP_PROJECT_LINK,
+  PROMPT_LIBRARY_PROXY_BASE_PATH_TEST,
+} from '@/global_constants';
 import AppLogo from '@/page_components/AppLogo';
 
 const footerBlackList = [
@@ -138,7 +142,7 @@ const AppFooter = () => {
                       label: t('modules:footer__1_click_prompts'),
                       // icon: <CustomIcon icon='PromptLogo' />,
                       icon: null,
-                      link: '/prompts/library',
+                      link: `${PROMPT_LIBRARY_PROXY_BASE_PATH_TEST}/library`,
                     },
                   ]}
                 />
