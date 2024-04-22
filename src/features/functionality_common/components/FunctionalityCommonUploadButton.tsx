d93cbb1d8ff7435f0ff3b6e992fccff330db704c
laizeping <laizeping@simplylab.xyz>
2024-04-22 10:44:23 +0800

    fix(src/features): fix Upload Style

--- src/features/functionality_common/components/FunctionalityCommonUploadButton.tsx
+++ src/features/functionality_common/components/FunctionalityCommonUploadButton.tsx
@@ -1,4 +1,4 @@
-import { Typography } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 import { useTranslation } from 'next-i18next';
 import React, { FC } from 'react';
 
@@ -15,37 +15,76 @@ const FunctionalityCommonUploadButton: FC<IUploadButtonProps> = (props) => {
   const { t } = useTranslation();
 
   return (
-    <UploadButton
-      buttonProps={{
-        sx: {
-          display: 'flex',
-          flexDirection: 'column',
-          height: 280,
-          width: 260,
-          alignItems: 'center',
-          justifyContent: 'center',
-          border: '1px dashed',
-        },
-        variant: 'outlined',
+    <Box
+      sx={{
+        width: '100%',
+        bgcolor: 'primary.main',
+        borderRadius: 2,
       }}
-      inputProps={{
-        accept: 'application/pdf',
-        multiple: true,
-      }}
-      {...props}
     >
-      <FunctionalityCommonIcon sx={{ fontSize: 34 }} name='CloudUploadIcon' />
-      <Typography
+      <Box
         sx={{
-          fontSize: {
-            xs: 12,
-            lg: 14,
-          },
+          border: '1px dashed #fff',
+          margin: 1,
+          borderRadius: 2,
+          height: 280,
         }}
       >
-        {t('functionality__pdf_to_image:components__index__upload_title')}
-      </Typography>
-    </UploadButton>
+        <UploadButton
+          buttonProps={{
+            fullWidth: true,
+            disableRipple: true,
+            sx: {
+              display: 'flex',
+              flexDirection: 'column',
+              height: '100%',
+              alignItems: 'center',
+              justifyContent: 'center',
+              '&:hover': {
+                bgcolor: 'transparent',
+              },
+            },
+            variant: 'contained',
+          }}
+          inputProps={{
+            accept: 'application/pdf',
+            multiple: true,
+          }}
+          {...props}
+        >
+          <FunctionalityCommonIcon
+            sx={{ fontSize: 68 }}
+            name='CloudUploadIcon'
+          />
+          <Button
+            variant='contained'
+            sx={{
+              my: 1,
+              width: 240,
+              height: 54,
+              bgcolor: '#ffffff',
+              color: '#000000',
+              fontSize: 16,
+              fontWeight: 700,
+            }}
+            startIcon={<FunctionalityCommonIcon name='NoteAdd' />}
+            color='inherit'
+          >
+            {t('functionality__common:components__common__upload_button_title')}
+          </Button>
+          <Typography
+            sx={{
+              fontSize: {
+                xs: 14,
+                lg: 16,
+              },
+            }}
+          >
+            {t('functionality__common:components__common__upload_button_tips')}
+          </Typography>
+        </UploadButton>
+      </Box>
+    </Box>
   );
 };
 
