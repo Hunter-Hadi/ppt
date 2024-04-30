7c0d330057c113ee20bb3df43d86cd6a482c89ea
zhouru1bin <109497098+zhouru1bin@users.noreply.github.com>
2024-04-30 23:44:13 +0800

    chore: contact us

--- src/components/ContactUsPanel.tsx
+++ src/components/ContactUsPanel.tsx
@@ -1,4 +1,3 @@
-import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import { Stack, Typography } from '@mui/material';
 import { useTranslation } from 'next-i18next';
@@ -64,7 +63,7 @@ const ContactUsPanel = () => {
         />{' '}
         .
       </Typography>
-      <Stack direction={'row'} gap={1} flexWrap='wrap'>
+      {/* <Stack direction={'row'} gap={1} flexWrap='wrap'>
         <Typography variant='body2' component={'div'}>
           👉 {t('pages:contact_us__item2')}{' '}
         </Typography>
@@ -89,7 +88,7 @@ const ContactUsPanel = () => {
           />
         </ProLink>{' '}
         .
-      </Typography>
+      </Typography> */}
       <Stack direction={'row'} gap={0.5}>
         <Typography variant='body2' component={'div'}>
           🎁 {t('pages:contact_us__item4')}{' '}
