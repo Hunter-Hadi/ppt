46d7da7e32999518e357398833dc15d1812eabdc
zhouru1bin <109497098+zhouru1bin@users.noreply.github.com>
2024-05-09 19:51:48 +0800

    chore: refine pdf page main

--- src/page_components/PdfToolsPages/components/ToolsHome.tsx
+++ src/page_components/PdfToolsPages/components/ToolsHome.tsx
@@ -1,8 +1,6 @@
 import { useTranslation } from 'next-i18next';
 import { useMemo } from 'react';
 
-import AppContainer from '@/app_layout/AppContainer';
-import AppDefaultSeoLayout from '@/app_layout/AppDefaultSeoLayout';
 import ToolsBanner from '@/page_components/PdfToolsPages/components/ToolsBanner';
 import ToolsCards from '@/page_components/PdfToolsPages/components/ToolsCards';
 import { toolsObjectData } from '@/page_components/PdfToolsPages/constant';
@@ -15,17 +13,13 @@ const ToolsHome = () => {
     [],
   );
   return (
-    <AppContainer sx={{ bgcolor: '#fff' }} maxWidth={1312}>
-      <AppDefaultSeoLayout
-        title={t('seo:pdf_tools__title')}
-        description={t('seo:pdf_tools__description')}
-      />
+    <>
       <ToolsBanner
         title={t('pages:tools__index_page__title')}
         description={t('pages:tools__index_page__description')}
       />
       <ToolsCards list={toolList} />
-    </AppContainer>
+    </>
   );
 };
 export default ToolsHome;
--- src/page_components/PdfToolsPages/index.tsx
+++ src/page_components/PdfToolsPages/index.tsx
@@ -1,3 +1,20 @@
+import { useTranslation } from 'next-i18next';
+
+import AppContainer from '@/app_layout/AppContainer';
+import AppDefaultSeoLayout from '@/app_layout/AppDefaultSeoLayout';
 import ToolsHome from '@/page_components/PdfToolsPages/components/ToolsHome';
 
-export default ToolsHome;
+const PdfToolsMainPages = () => {
+  const { t } = useTranslation();
+
+  return (
+    <AppContainer sx={{ bgcolor: '#fff' }} maxWidth={1312}>
+      <AppDefaultSeoLayout
+        title={t('seo:pdf_tools__title')}
+        description={t('seo:pdf_tools__description')}
+      />
+      <ToolsHome />
+    </AppContainer>
+  );
+};
+export default PdfToolsMainPages;
