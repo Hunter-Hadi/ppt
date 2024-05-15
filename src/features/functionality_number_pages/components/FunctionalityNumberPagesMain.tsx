5a29c87875a03b126d67edd393386709acb08a39
laizeping <laizeping@simplylab.xyz>
2024-05-15 16:50:21 +0800

    feat: number pages view

--- src/features/functionality_number_pages/components/FunctionalityNumberPagesMain.tsx
+++ src/features/functionality_number_pages/components/FunctionalityNumberPagesMain.tsx
@@ -1,32 +1,154 @@
-import { Box, Grid, Stack, Typography } from '@mui/material';
+import {
+  Box,
+  Grid,
+  MenuItem,
+  Select,
+  Stack,
+  TextField,
+  Typography,
+} from '@mui/material';
+import { useState } from 'react';
 
 const FunctionalityNumberPagesMain = () => {
-  return (
-    <Box>
-      <Grid container>
-        <Grid item>
-          <Typography>Position</Typography>
+  const [positionValue, setPositionValue] = useState('topLeft');
+  const [marginsValue, setMarginsValue] = useState('Default');
+  const [startNumberValue, setStartNumberValue] = useState(1);
+
+  const circleGridView = (
+    activeType: string = 'top',
+    viewSize: number = 150,
+    onChange: (value: string) => void,
+  ) => {
+    const viewPositionsList = [
+      ['topLeft', 'top', 'topRight'],
+      ['bottomLeft', 'bottom', 'bottomRight'],
+    ];
+    const borderColor = '#d1d1d1';
+    return (
+      <Stack
+        direction='column'
+        justifyContent='space-between'
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          border: `1px solid ${borderColor}`,
+          backgroundColor: '#f9f9f9',
+          height: viewSize,
+          width: viewSize,
+        }}
+      >
+        {viewPositionsList.map((viewPositions, index) => (
           <Stack
             direction='row'
             justifyContent='space-between'
+            key={index}
             sx={{
-              border: '1px solid red',
-              height: 200,
-              width: 200,
-              flexWrap: 'wrap',
+              height: '33%',
+              borderBottom: index === 0 ? `1px solid ${borderColor}` : '',
+              borderTop: index === 1 ? `1px solid ${borderColor}` : '',
             }}
           >
-            {Array.from({ length: 6 }).map((_, index) => (
+            {viewPositions.map((viewPosition, index) => (
               <Box
+                onClick={() => onChange(viewPosition)}
                 key={index}
                 sx={{
-                  border: '1px solid #f6f6f6',
-                  width: 'calc(33% - 2px)',
-                  height: '33%',
+                  cursor: 'pointer',
+                  width: '33%',
+                  height: '100%',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  borderRight: index !== 2 ? `1px solid ${borderColor}` : '',
+                  backgroundColor: '#fff',
                 }}
-              ></Box>
+              >
+                <Stack
+                  direction='column'
+                  alignItems='center'
+                  justifyContent='center'
+                  sx={{
+                    width: '40%',
+                    height: '40%',
+                    borderRadius: '50%',
+                    border:
+                      viewPosition === activeType
+                        ? '2px solid #9065B0'
+                        : '1px solid gray',
+                  }}
+                >
+                  <Box
+                    sx={{
+                      width: 'calc(100% - 3px)',
+                      height: 'calc(100% - 3px)',
+                      borderRadius: '50%',
+                      bgcolor:
+                        viewPosition === activeType ? 'primary.main' : '',
+                    }}
+                  />
+                </Stack>
+              </Box>
             ))}
           </Stack>
+        ))}
+      </Stack>
+    );
+  };
+  return (
+    <Box>
+      <Grid container gap={1}>
+        <Grid item>
+          <Typography variant='custom' fontSize={18}>
+            Position
+          </Typography>
+          {circleGridView(positionValue, 130, (value) => {
+            setPositionValue(value);
+          })}
+        </Grid>
+        <Grid
+          item
+          sx={{
+            flex: 1,
+          }}
+          display='flex'
+          direction='column'
+          justifyContent='space-between'
+        >
+          <Box sx={{ width: '100%' }}>
+            <Typography variant='custom' fontSize={18}>
+              Margins
+            </Typography>
+            <Select
+              sx={{
+                width: '100%',
+              }}
+              labelId='demo-simple-select-label'
+              id='demo-simple-select'
+              value={marginsValue}
+              size='small'
+              onChange={(event) => setMarginsValue(event.target.value)}
+            >
+              <MenuItem value='Narrow'>Narrow</MenuItem>
+              <MenuItem value='Default'>Default</MenuItem>
+              <MenuItem value='Wide'>Wide</MenuItem>
+            </Select>
+          </Box>
+          <Box>
+            <Typography variant='custom' fontSize={18}>
+              Start numbering at
+            </Typography>
+            <Box>
+              <TextField
+                size='small'
+                type='number'
+                placeholder='input start numbering at'
+                value={startNumberValue}
+                onChange={(event) =>
+                  setStartNumberValue(Number(event.target.value))
+                }
+              />
+            </Box>
+          </Box>
         </Grid>
       </Grid>
     </Box>
