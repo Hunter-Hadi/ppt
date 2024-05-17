b29b2637ec405690d80933af13a107afb1e9d2c5
zhouru1bin <109497098+zhouru1bin@users.noreply.github.com>
2024-05-17 10:53:32 +0800

    chore: tesing

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,72 +1,67 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-import {
-  LANDING_VARIANT,
-  TEST_LANDING_COOKIE_NAME,
-  TESTER_LANDING_PATH_TARGET_PATHNAME,
-} from '@/features/ab_tester/constant/landingVariant';
-import { isTargetTestPathname } from '@/features/ab_tester/utils';
-
 // import { isLandingPagePathname } from '@/features/ab_tester/utils';
 
 export async function middleware(req: NextRequest) {
   const url = req.nextUrl;
+
+  return NextResponse.redirect(new URL('/pricing', url));
 
   // const { isBot } = userAgent(req);
   // if (isBot) {
   //   return NextResponse.next();
   // }
 
-  req.cookies.set('zztest-1', '1');
+  // req.cookies.set('zztest-1', '1');
 
-  // 目前只有 landing page 需要 variant （A/B test）
-  if (isTargetTestPathname(url.pathname, TESTER_LANDING_PATH_TARGET_PATHNAME)) {
-    req.cookies.set('zztest-2', '2');
+  // // 目前只有 landing page 需要 variant （A/B test）
+  // if (isTargetTestPathname(url.pathname, TESTER_LANDING_PATH_TARGET_PATHNAME)) {
+  //   req.cookies.set('zztest-2', '2');
 
-    const variantCookie = req.cookies.get(TEST_LANDING_COOKIE_NAME)?.value;
+  //   const variantCookie = req.cookies.get(TEST_LANDING_COOKIE_NAME)?.value;
 
-    let cacheVariant = variantCookie;
+  //   let cacheVariant = variantCookie;
 
-    let newLandingVariantData = LANDING_VARIANT.find(
-      (v) => v.variant === cacheVariant,
-    );
-    if (!newLandingVariantData) {
-      req.cookies.set('zztest-3', '3');
+  //   let newLandingVariantData = LANDING_VARIANT.find(
+  //     (v) => v.variant === cacheVariant,
+  //   );
+  //   if (!newLandingVariantData) {
+  //     req.cookies.set('zztest-3', '3');
 
-      const landingVariantMap = LANDING_VARIANT;
-      // 随机选择一个 variant
-      const randomIndex = Date.now() % landingVariantMap.length;
-      const randomVariant = landingVariantMap[randomIndex];
-      newLandingVariantData = randomVariant;
-      req.cookies.set(TEST_LANDING_COOKIE_NAME, newLandingVariantData.variant);
-    }
+  //     const landingVariantMap = LANDING_VARIANT;
+  //     // 随机选择一个 variant
+  //     const randomIndex = Date.now() % landingVariantMap.length;
+  //     const randomVariant = landingVariantMap[randomIndex];
+  //     newLandingVariantData = randomVariant;
+  //     req.cookies.set(TEST_LANDING_COOKIE_NAME, newLandingVariantData.variant);
+  //   }
 
-    const response =
-      newLandingVariantData && newLandingVariantData.pathname
-        ? NextResponse.redirect(new URL(newLandingVariantData.pathname, url))
-        : NextResponse.next();
+  //   const response =
+  //     newLandingVariantData && newLandingVariantData.pathname
+  //       ? NextResponse.redirect(new URL(newLandingVariantData.pathname, url))
+  //       : NextResponse.next();
 
-    if (newLandingVariantData) {
-      req.cookies.set('zztest-4', '4');
+  //   if (newLandingVariantData) {
+  //     req.cookies.set('zztest-4', '4');
 
-      response.cookies.set(
-        TEST_LANDING_COOKIE_NAME,
-        newLandingVariantData.variant,
-      );
+  //     response.cookies.set(
+  //       TEST_LANDING_COOKIE_NAME,
+  //       newLandingVariantData.variant,
+  //     );
 
-      response.cookies.set('res-zztest-1', `1-${url.pathname}`);
-    }
+  //     response.cookies.set('res-zztest-1', `1-${url.pathname}`);
+  //   }
 
-    req.cookies.set('zztest-5', '5');
+  //   req.cookies.set('zztest-5', '5');
 
-    response.cookies.set('res-zztest-2', `2-${url.pathname}`);
+  //   response.cookies.set('res-zztest-2', `2-${url.pathname}`);
 
-    return response;
-  }
+  //   return response;
+  // }
 
-  req.cookies.set('zztest-6', '6');
+  // req.cookies.set('zztest-6', '6');
 
-  return NextResponse.next();
+  // return NextResponse.next();
 }
 
 export const config = {
