15f7b2b557a4d0d1ccdfb4d845a1bca485773e7e
zhouru1bin <109497098+zhouru1bin@users.noreply.github.com>
2024-05-17 19:02:10 +0800

    chore: remove code

--- src/features/ab_tester/constant/landingVariant.ts
+++ src/features/ab_tester/constant/landingVariant.ts
@@ -14,40 +14,31 @@ export const TEST_LANDING_COOKIE_NAME = 'maxai-lpv';
 
 export type ILandingVariantData = {
   variant: ILandingVariantType;
-  pathname: string;
 };
 
 export const LANDING_VARIANT: ILandingVariantData[] = [
   {
     variant: 'title1__desc1__features_carousel',
-    pathname: '/lp/v1',
   },
   {
     variant: 'title1__desc1__features_expand',
-    pathname: '/lp/v2',
   },
   {
     variant: 'title1__desc2__features_carousel',
-    pathname: '/lp/v3',
   },
   {
     variant: 'title1__desc2__features_expand',
-    pathname: '/lp/v4',
   },
   {
     variant: 'title2__desc1__features_carousel',
-    pathname: '/lp/v5',
   },
   {
     variant: 'title2__desc1__features_expand',
-    pathname: '/lp/v6',
   },
   {
     variant: 'title2__desc2__features_carousel',
-    pathname: '/lp/v7',
   },
   {
     variant: 'title2__desc2__features_expand',
-    pathname: '/lp/v8',
   },
 ];
