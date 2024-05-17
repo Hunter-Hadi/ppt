6a511e2166014777608e6cb962fe70e16b472754
zhouru1bin <109497098+zhouru1bin@users.noreply.github.com>
2024-05-17 10:33:48 +0800

    chore: testing

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse, userAgent } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 import {
   LANDING_VARIANT,
@@ -12,13 +12,17 @@ import { isTargetTestPathname } from '@/features/ab_tester/utils';
 export async function middleware(req: NextRequest) {
   const url = req.nextUrl;
 
-  const { isBot } = userAgent(req);
-  if (isBot) {
-    return NextResponse.next();
-  }
+  // const { isBot } = userAgent(req);
+  // if (isBot) {
+  //   return NextResponse.next();
+  // }
+
+  req.cookies.set('zztest-1', '1');
 
   // 目前只有 landing page 需要 variant （A/B test）
   if (isTargetTestPathname(url.pathname, TESTER_LANDING_PATH_TARGET_PATHNAME)) {
+    req.cookies.set('zztest-2', '2');
+
     const variantCookie = req.cookies.get(TEST_LANDING_COOKIE_NAME)?.value;
 
     let cacheVariant = variantCookie;
@@ -27,6 +31,8 @@ export async function middleware(req: NextRequest) {
       (v) => v.variant === cacheVariant,
     );
     if (!newLandingVariantData) {
+      req.cookies.set('zztest-3', '3');
+
       const landingVariantMap = LANDING_VARIANT;
       // 随机选择一个 variant
       const randomIndex = Date.now() % landingVariantMap.length;
@@ -41,14 +47,20 @@ export async function middleware(req: NextRequest) {
         : NextResponse.next();
 
     if (newLandingVariantData) {
+      req.cookies.set('zztest-4', '4');
+
       response.cookies.set(
         TEST_LANDING_COOKIE_NAME,
         newLandingVariantData.variant,
       );
     }
 
+    req.cookies.set('zztest-5', '5');
+
     return response;
   }
+
+  req.cookies.set('zztest-6', '6');
 
   return NextResponse.next();
 }
