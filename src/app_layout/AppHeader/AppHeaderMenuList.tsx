14969e9a423a07f1d92f8428e11997821b9fce0d
zhouru1bin <109497098+zhouru1bin@users.noreply.github.com>
2024-06-21 11:39:45 +0800

    fix: bugs

--- src/app_layout/AppHeader/AppHeaderMenuList.tsx
+++ src/app_layout/AppHeader/AppHeaderMenuList.tsx
@@ -1,7 +1,6 @@
 import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
 import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
 import { Box, Drawer, IconButton, MenuList, Stack } from '@mui/material';
-import { useRouter } from 'next/router';
 import React, { FC } from 'react';
 
 import useAppHeaderState from '@/hooks/useAppHeaderState';
@@ -16,11 +15,11 @@ interface IProps {
 
 const AppHeaderMenuList: FC<IProps> = ({ isSmallScreen = false }) => {
   const [open, setOpen] = React.useState(false);
-  const { isReady } = useRouter();
 
   const { appHeaderHeight } = useAppHeaderState();
 
   if (isSmallScreen) {
+    // 小屏幕
     return (
       <Box ml={2}>
         <IconButton onClick={() => setOpen((pre) => !pre)}>
@@ -46,22 +45,16 @@ const AppHeaderMenuList: FC<IProps> = ({ isSmallScreen = false }) => {
         </Drawer>
       </Box>
     );
+  } else {
+    // 大屏幕
+    return (
+      <Stack direction={'row'} spacing={1.5} alignItems='center'>
+        {APP_HEADER_MENU_LIST.map((item) => (
+          <AppHeaderMenuItem key={item} menuKey={item} />
+        ))}
+      </Stack>
+    );
   }
-
-  return (
-    <Stack
-      direction={'row'}
-      spacing={1.5}
-      alignItems='center'
-      sx={{
-        display: isReady ? 'flex' : 'none',
-      }}
-    >
-      {APP_HEADER_MENU_LIST.map((item) => (
-        <AppHeaderMenuItem key={item} menuKey={item} />
-      ))}
-    </Stack>
-  );
 };
 
 export default AppHeaderMenuList;
