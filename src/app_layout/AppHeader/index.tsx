da401373be97355dc78a4c61133e450f3e9336cf
zhouru1bin <109497098+zhouru1bin@users.noreply.github.com>
2024-07-02 17:44:30 +0800

    fix: build error

--- src/app_layout/AppHeader/index.tsx
+++ src/app_layout/AppHeader/index.tsx
@@ -1,12 +1,12 @@
-import { Box, Divider, Toolbar, useMediaQuery } from '@mui/material';
+import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
+import useMediaQuery from '@mui/material/useMediaQuery';
 import { useRouter } from 'next/router';
 import { FC } from 'react';
 
 import useAppHeaderState from '@/hooks/useAppHeaderState';
 import { removeLocaleInPathname } from '@/i18n/utils';
 import AppBar from '@/packages/base-ui/components/AppBar';
-import AppLogo from '@/page_components/AppLogo';
 
 import AppHeaderCTABtn from './AppHeaderCTABtn';
 import AppHeaderMenuList from './AppHeaderMenuList';
@@ -49,57 +49,6 @@ const AppHeader: FC = () => {
       }
       onHeightChange={setAppHeaderHeight}
     />
-  );
-
-  return (
-    <AppBar
-      id={APP_HEADER_ID}
-      ref={headerElement}
-      component={'header'}
-      position={'sticky'}
-      sx={{
-        bgcolor: 'background.paper',
-        zIndex: (t) => t.zIndex.drawer + 10,
-        boxShadow: (t) => (t.palette.mode === 'dark' ? 1 : 'none'),
-      }}
-    >
-      <Toolbar
-        disableGutters
-        sx={{
-          boxSizing: 'border-box',
-          color: 'text.primary',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-          maxWidth: 1312,
-          mx: 'auto',
-          px: 2,
-          py: {
-            xs: 1,
-            sm: 2,
-          },
-        }}
-      >
-        <AppLogo
-          sx={{
-            pb: '2px',
-          }}
-        />
-
-        <Box pl={2.5} />
-
-        {!isMiniMenu ? <AppHeaderMenuList /> : null}
-
-        <Box flex={1} />
-
-        {/* <Box pl={2} /> */}
-
-        <AppHeaderCTABtn isSmallScreen={isMiniMenu} />
-
-        {isMiniMenu ? <AppHeaderMenuList isSmallScreen /> : null}
-      </Toolbar>
-      <Divider />
-    </AppBar>
   );
 };
 export default AppHeader;
