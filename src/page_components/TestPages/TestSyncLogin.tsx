c10a633419befdcd1852f36b29d85098e3c90f0d
chenyang <chenyang@simplylab.xyz>
2024-07-03 22:34:34 +0800

    feta: supported local test

--- src/page_components/TestPages/TestSyncLogin.tsx
+++ src/page_components/TestPages/TestSyncLogin.tsx
@@ -11,6 +11,8 @@ import {
 
 import AppDefaultSeoLayout from '@/app_layout/AppDefaultSeoLayout';
 import AppLoadingLayout from '@/app_layout/AppLoadingLayout';
+import AuthAvatar from '@/packages/auth/components/AuthAvatar';
+import AppBar from '@/packages/base-ui/components/AppBar';
 import {
   COMMON_MAXAI_API_HOST,
   COMMON_MAXAI_APP_PROJECT_HOST,
@@ -30,13 +32,19 @@ const TestSyncLogin = () => {
       <Typography>app: {COMMON_MAXAI_APP_PROJECT_HOST}</Typography>
       <Typography>www: {COMMON_MAXAI_WWW_PROJECT_HOST}</Typography>
       <Typography>api: {COMMON_MAXAI_API_HOST}</Typography>
-      <Typography>error: {error}</Typography>
-      <LoadingButton onClick={connectMaxAIAccount} loading={loading}>
-        Login
-      </LoadingButton>
+      {!isLogin && (
+        <>
+          <Typography>error: {error}</Typography>
+          <LoadingButton onClick={connectMaxAIAccount} loading={loading}>
+            Login
+          </LoadingButton>
+        </>
+      )}
       <AppLoadingLayout loading={!isLogin} sx={{ minHeight: '90vh' }}>
         <AppDefaultSeoLayout />
         <Stack>email: {userProfile?.email}</Stack>
+        <AppBar />
+        <AuthAvatar />
         <Stack>
           <Button onClick={handleLogout}>Logout</Button>
         </Stack>
