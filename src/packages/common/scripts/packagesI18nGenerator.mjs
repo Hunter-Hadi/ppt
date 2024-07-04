326f471c765c2ca0f5f9b797f4d6fb6112012877
chenyang <chenyang@simplylab.xyz>
2024-07-04 19:05:55 +0800

    chore: update settings

--- src/packages/common/scripts/packagesI18nGenerator.mjs
+++ src/packages/common/scripts/packagesI18nGenerator.mjs
@@ -10,9 +10,9 @@ const safeReadJson = (filePath) => {
   try {
     return JSON.parse(fs.readFileSync(filePath, 'utf8'));
   } catch (error) {
-    return null
+    return null;
   }
-}
+};
 
 
 const updateI18n = async (packageNames = []) => {
@@ -33,7 +33,7 @@ const updateI18n = async (packageNames = []) => {
       if (!i18n) {
         hasError = true;
         console.log(`❌ Package [${packageName}] i18n 文件格式不正确`);
-        return
+        return;
       }
       const keys = Object.keys(i18n);
       const packageKey = keys[0];
@@ -41,38 +41,38 @@ const updateI18n = async (packageNames = []) => {
       if (keys.length > 1 || !packageKey.startsWith('package__')) {
         hasError = true;
         console.log(`❌ Package [${packageName}] i18n 格式不正确, key: [${packageKey}], keys: [${keys}]`);
-        return
+        return;
       }
       // 如果合并后的 i18n 文件中已经存在相同的 key，就不合并
       if (mergedI18n.hasOwnProperty(packageKey)) {
         hasError = true;
         console.log(`❌ Package [${packageName}] i18n key 重复, key: [${packageKey}]`);
-        return
+        return;
       }
       successPackages.push(packageName);
       mergedI18n = {
         ...mergedI18n,
-        ...i18n
+        ...i18n,
       };
     }
   });
   // 写入 i18n 文件
   const currentI18nPath = path.join(rootPath, i18nPath);
-  const cache = safeReadJson(currentI18nPath)
+  const cache = safeReadJson(currentI18nPath);
   if (!cache) {
     hasError = true;
-    console.log('❌ i18n 文件格式不正确')
-    return
+    console.log('❌ i18n 文件格式不正确');
+    return;
   }
   if (hasError) {
-    return
+    return;
   }
   // 删掉原来的 package__ 开头的 key
   Object.keys(cache).forEach((key) => {
     if (key.startsWith('package__')) {
       delete cache[key];
     }
-  })
+  });
   // 排序mergedI18n
   const sortedKeys = Object.keys(mergedI18n).sort();
   const sortedMergedI18n = {};
@@ -82,32 +82,36 @@ const updateI18n = async (packageNames = []) => {
   // 写入新的 i18n 文件
   const newI18n = {
     ...cache,
-    ...sortedMergedI18n
+    ...sortedMergedI18n,
   };
   fs.writeFileSync(currentI18nPath, JSON.stringify(newI18n, null, 2));
-  console.log(`✅ Package [${packageNames.join(', ')}] 合并成功`)
+  console.log(`✅ Package [${packageNames.join(', ')}] 合并成功`);
 };
 
+const params = process.argv.slice(2);
+
 const startListening = () => {
+  const isWatch = params.includes('--watch');
   // 检测是否存在 packages 文件夹
   const currentPackagesPath = path.join(rootPath, packagesPath);
   const currentI18nPath = path.join(rootPath, i18nPath);
-  console.log(currentPackagesPath);
   if (
     fs.existsSync(currentPackagesPath) &&
     fs.existsSync(currentI18nPath)
   ) {
-    // 监听 packages/[package]/i18n 文件夹
-    fs.watch(currentPackagesPath, { recursive: true }, (eventType, filename) => {
-      if (filename) {
-        if (eventType === 'change' && filename.endsWith('i18n/index.json')) {
-          const [packageName, ...rest] = filename.split('/')
-          console.log(`Package [${packageName}] i18n file changed`);
-          updateI18n()
+    if (isWatch) {
+      // 监听 packages/[package]/i18n 文件夹
+      fs.watch(currentPackagesPath, { recursive: true }, (eventType, filename) => {
+        if (filename) {
+          if (eventType === 'change' && filename.endsWith('i18n/index.json')) {
+            const [packageName, ...rest] = filename.split('/');
+            console.log(`Package [${packageName}] i18n file changed`);
+            updateI18n();
+          }
         }
-      }
-    });
-    updateI18n()
+      });
+    }
+    updateI18n();
   } else {
     // 检测是否存在 i18n 文件夹
     console.log('packages/i18n 文件夹不存在');
