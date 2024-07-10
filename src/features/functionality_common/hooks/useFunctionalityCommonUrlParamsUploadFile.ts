5f01513a8272b48bca0904556081fdf6fcdeec5b
laizeping <laizeping@simplylab.xyz>
2024-07-10 18:59:06 +0800

    feat(functionality_common): add auto url params upload

--- src/features/functionality_common/hooks/useFunctionalityCommonUrlParamsUploadFile.ts
+++ src/features/functionality_common/hooks/useFunctionalityCommonUrlParamsUploadFile.ts
@@ -1,39 +1,48 @@
-import { useEffect, useRef, useState } from 'react'
-
-import { downloadFileAsBlob } from '../utils/functionalityCommonDownload'
-import { clientGetMaxAIFileUrlWithFileId } from '../request/file'
 import { useRouter } from 'next/router'
+import { useEffect, useMemo, useRef, useState } from 'react'
+
+import { clientGetMaxAIFileUrlWithFileId } from '../request/file'
+import { downloadFileAsBlob } from '../utils/functionalityCommonDownload'
 const useFunctionalityCommonUrlParamsUploadFile = (props: {
   onChangeFile?: (file: FileList) => void
 }) => {
   const router = useRouter()
-  const { file_id, file_name } = router.query //这里是获取url参数,但获取不到，保险也加上一层
-  const url = new URL(router.asPath, window.location.origin)
-  const fileId = url.searchParams.get('file_id')
-  const fileName = url.searchParams.get('file_name')
+  const { file_id, name } = router.query //这里是获取url参数,但会获取不到，保险也加上一层
   const isHandleParams = useRef(false)
   const [urlFileUploadProgress, setUrlFileUploadProgress] = useState<
     null | number
   >(null)
+  const { url, fileId, getName } = useMemo(() => {
+    const url = new URL(router.asPath, window.location.origin)
+    const fileId = url.searchParams.get('file_id')
+    const getName = url.searchParams.get('name')
+    return {
+      url,
+      fileId,
+      getName,
+    }
+  }, [])
+  const fileName = name || getName
   const errorFile = () => {
     setUrlFileUploadProgress(null)
     router.replace(url.pathname, undefined, { shallow: false }) //重置url防止触发触发这个hooks
   }
   useEffect(() => {
     if (isHandleParams.current) return
+
     isHandleParams.current = true
 
     if ((file_id || fileId) as string) {
       setUrlFileUploadProgress(0) //这里是为了处理上传进度条
       clientGetMaxAIFileUrlWithFileId((file_id || fileId) as string)
         .then((res) => {
-          console.log('clientGetMaxAIFileUrlWithFileId', res)
           if (res?.file_url) {
             downloadFileAsBlob(
               decodeURIComponent(res?.file_url),
               (progress) => {
                 setUrlFileUploadProgress(progress) //更新上传进度条
               },
+              fileName as string,
             )
               .then((file) => {
                 props.onChangeFile &&
@@ -55,7 +64,7 @@ const useFunctionalityCommonUrlParamsUploadFile = (props: {
   }, [])
   return {
     urlFileUploadProgress,
-    fileName: file_name || fileName,
+    fileName,
   }
 }
 export default useFunctionalityCommonUrlParamsUploadFile
--- src/features/functionality_common/request/index.ts
+++ src/features/functionality_common/request/index.ts
@@ -1,31 +1,19 @@
-import { API_PROJECT_LINK } from '@/global_constants'
+import { getAccessToken } from '@/packages/auth'
 
-type ClientFetchAPIResponse<T> = {
-  success: boolean
-  data: T | null
-  responseRaw: {
-    ok: boolean
-    status: number
-    statusText: string
-    url: string
-    redirected: boolean
-  } | null
-  message: string
-  error: string
-}
 const API_HOST = process.env.NEXT_PUBLIC_BASE_URL
 export interface IResponse<T> {
   status: 'OK' | 'ERROR'
   data?: T
   msg?: string
 } //有的返回这个格式，有的不返回，所以目前让接口自己选择
-export const post = <T, T_MORE = {}>(
+export const post = <T, T_MORE = object>(
   pathname: string,
   data: any,
   options?: RequestInit,
   headers?: HeadersInit,
   isJson = true, //是否是json格式
 ): Promise<T & T_MORE> => {
+  const token = getAccessToken()
   return new Promise<any>((resolve, reject) => {
     if (isJson) {
       headers = {
@@ -36,7 +24,7 @@ export const post = <T, T_MORE = {}>(
     fetch(API_HOST + pathname, {
       method: 'POST',
       headers: {
-        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWJqZWN0Ijp7InVzZXJfaWQiOiIyNzc3OWRmMy0wMWJhLTQxMTMtYTQyNi03ZDAwZjM3MTAyOTgifSwidHlwZSI6ImFjY2VzcyIsImV4cCI6MTcyMDYwODY4MCwiaWF0IjoxNzIwNTIyMjgwLCJqdGkiOiJmMTgzMjNkYS0zODY1LTRhOTktOGIxNi1mY2FkOTJmNmVhMTUifQ.o1AaqnxjT2Ma1UIDJ4f6B6USdMraUjAJJn_NgAg5sb4`,
+        Authorization: `Bearer ${token}`,
         ...headers,
       },
       body: isJson ? JSON.stringify(data) : data,
