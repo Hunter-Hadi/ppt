29b3e0565501f9f95a30cc084e08852a66b77766
zhouru1bin <109497098+zhouru1bin@users.noreply.github.com>
2024-07-10 17:07:07 +0800

    chore: update sitemap

--- scripts/refreshSitemap.mjs
+++ scripts/refreshSitemap.mjs
@@ -1,15 +1,20 @@
-import axios from 'axios';
-import fs from 'fs';
-import path from 'path';
+import axios from 'axios'
+import fs from 'fs'
+import path from 'path'
 
-import languageCodeMap from '../src/packages/common/constants/languageCodeMap.json' assert { type: 'json' };
-import pdfToolsCodeMap from '../src/page_components/PdfToolsPages/constant/pdfToolsCodeMap.json' assert { type: 'json' };
+import nextConfig from '../next.config.js'
+import languageCodeMap from '../src/packages/common/constants/languageCodeMap.json' assert { type: 'json' }
+import pdfToolsCodeMap from '../src/page_components/PdfToolsPages/constant/pdfToolsCodeMap.json' assert { type: 'json' }
 
-const IS_PROD = true;
+const IS_PROD = false
 
-const pagesDirectory = 'src/pages';
-const sitemapAssetsPath = './public/sitemap.xml'; // sitemap 文件的存储路径
-const wwwDomain = 'https://www.maxai.me';
+const pagesDirectory = 'src/pages'
+const sitemapAssetsPath = './public/sitemap.xml' // sitemap 文件的存储路径
+const wwwDomain = 'https://www.maxai.me'
+const chatpdfUrls = ['/tools/chatpdf']
+
+// prompt library 的代理路径
+const PROMPT_LIBRARY_PROXY_BASE_PATH = '/prompt'
 
 // 判断当前 file 是否需要插入到 sitemap
 function checkDoNeedToGenerateSitemap(file) {
@@ -28,36 +33,37 @@ function checkDoNeedToGenerateSitemap(file) {
     /survey/,
     /dev/,
     /email-unsubscribe-success/,
-  ];
-  return !excludePattern.some((pattern) => pattern.test(file));
+    /\.DS_Store/,
+  ]
+  return !excludePattern.some((pattern) => pattern.test(file))
 }
 
 function generateStaticPagesWithLocale(pagePaths) {
   // 过滤一些 没有 i18n routing 的页面
-  const excludePaths = ['/partners', '/install'];
-  const fixPagePaths = pagePaths.filter((page) => !excludePaths.includes(page));
+  const excludePaths = ['/partners', '/install']
+  const fixPagePaths = pagePaths.filter((page) => !excludePaths.includes(page))
 
-  const localeCode = Object.keys(languageCodeMap);
-  const pageWithLocale = [];
+  const localeCode = Object.keys(languageCodeMap)
+  const pageWithLocale = []
   localeCode.forEach((locale) => {
     fixPagePaths.forEach((path) => {
-      pageWithLocale.push(`/${locale}${path}`);
-    });
-  });
+      pageWithLocale.push(`/${locale}${path}`)
+    })
+  })
 
-  return pageWithLocale;
+  return pageWithLocale
 }
 
 // 递归遍历目录
 function crawlStaticDirectory(dir) {
   // 用于存储页面路径的数组
-  const staticPages = [];
-  const files = fs.readdirSync(dir);
+  const staticPages = []
+  const files = fs.readdirSync(dir)
 
   for (let i = 0; i < files.length; i++) {
-    const file = files[i];
-    const filePath = path.join(dir, file);
-    const stat = fs.statSync(filePath);
+    const file = files[i]
+    const filePath = path.join(dir, file)
+    const stat = fs.statSync(filePath)
 
     if (
       // 特殊文件或目录
@@ -67,11 +73,11 @@ function crawlStaticDirectory(dir) {
       // prompts 目录不需要这里处理
       file === 'prompts'
     ) {
-      continue;
+      continue
     }
 
     if (stat.isDirectory()) {
-      staticPages.push(...crawlStaticDirectory(filePath));
+      staticPages.push(...crawlStaticDirectory(filePath))
     } else {
       // 将文件路径转换为网址路径，并添加到 pages 数组
       const fixedFilePath = filePath
@@ -79,44 +85,45 @@ function crawlStaticDirectory(dir) {
         .replace('.js', '')
         .replace('.jsx', '')
         .replace('.tsx', '')
-        .replace('/index', '');
+        .replace('/index', '')
 
-      staticPages.push(fixedFilePath);
+      staticPages.push(fixedFilePath)
     }
   }
 
-  return staticPages;
+  console.log(`
+  \x1b[32m staticPages: ${staticPages.length} \x1b[0m,
+`)
+  return staticPages
 }
 
 // 获取 prompts 页面
 async function generatePromptsPages() {
-  // 注意这里的 PROMPT_LIBRARY_PROXY_BASE_PATH
-  const PROMPT_LIBRARY_PROXY_BASE_PATH = '/prompt';
-  const promptDetailPathnamePrefix = `/library`;
+  const promptDetailPathnamePrefix = `/library`
 
   // 默认存在 prompt library 首页页面
-  let promptPages = [`/library`];
+  let promptPages = [`/library`]
 
   const listApiGetAllData = async (
     listApiFetcher,
     startPage = 0,
     pageSize = 100,
   ) => {
-    const totalData = [];
-    let page = startPage;
-    const listApiResponse = await listApiFetcher(page, pageSize);
-    let data = listApiResponse.data;
+    const totalData = []
+    let page = startPage
+    const listApiResponse = await listApiFetcher(page, pageSize)
+    let data = listApiResponse.data
     while (data && data.data && data.data.length > 0) {
-      totalData.push(...data.data);
-      page++;
-      const listApiResponse = await listApiFetcher(page, pageSize);
-      data = listApiResponse.data;
+      totalData.push(...data.data)
+      page++
+      const listApiResponse = await listApiFetcher(page, pageSize)
+      data = listApiResponse.data
     }
-    return totalData;
-  };
+    return totalData
+  }
 
   try {
-    const PROMPTS_API = `https://api.maxai.me/prompt/search_prompt`;
+    const PROMPTS_API = `https://api.maxai.me/prompt/search_prompt`
 
     const promptList = await listApiGetAllData(
       (page, page_size) =>
@@ -125,92 +132,158 @@ async function generatePromptsPages() {
           page_size,
         }),
       0,
-    );
+    )
 
     promptList.forEach((prompt) => {
-      promptPages.push(`${promptDetailPathnamePrefix}/${prompt.id}`);
-    });
+      promptPages.push(`${promptDetailPathnamePrefix}/${prompt.id}`)
+    })
 
-    promptPages.push(...generateStaticPagesWithLocale(promptPages));
+    promptPages.push(...generateStaticPagesWithLocale(promptPages))
 
     promptPages = promptPages.map((page) => {
-      return `${PROMPT_LIBRARY_PROXY_BASE_PATH}${page}`;
-    });
+      return `${PROMPT_LIBRARY_PROXY_BASE_PATH}${page}`
+    })
 
-    return promptPages;
+    return promptPages
   } catch (error) {
-    console.log(`generatePromptsPages ERROR:`, error);
+    console.log(`generatePromptsPages ERROR:`, error)
   }
 }
 
 async function generateToolsPages() {
   const toolsPages = Object.keys(pdfToolsCodeMap.childrenObject).map(
-    (url) => `${pdfToolsCodeMap.topUrlKey}/${url}`,
-  );
+    (url) => `/${pdfToolsCodeMap.topUrlKey}/${url}`,
+  )
 
-  return toolsPages.concat(generateStaticPagesWithLocale(toolsPages));
+  return toolsPages.concat(generateStaticPagesWithLocale(toolsPages))
 }
+
+function addHrefLangToSitemap(propPath) {
+  const excludePaths = ['/tools/chatpdf']
+  if (excludePaths.some((excludePath) => propPath.includes(excludePath))) {
+    return ''
+  }
+
+  // const localeCodes = Object.keys(languageCodeMap)
+  const localeCodes = ['zh-CN', 'en-US']
+
+  // 删除 url 上的 locale
+  const inUrlLocale = localeCodes.find((locale) =>
+    propPath.includes(`/${locale}/`),
+  )
+  const url = propPath.replace(`/${inUrlLocale}`, '')
+
+  let hrefLangs = `<xhtml:link rel="alternate" hreflang="x-default" href="${wwwDomain}${url}" />`
+
+  if (propPath.startsWith(PROMPT_LIBRARY_PROXY_BASE_PATH)) {
+    const pathname = url.replace(PROMPT_LIBRARY_PROXY_BASE_PATH, '')
+
+    localeCodes.forEach((locale) => {
+      hrefLangs += `<xhtml:link rel="alternate" hreflang="${locale}" href="${wwwDomain}${PROMPT_LIBRARY_PROXY_BASE_PATH}/${locale}${pathname}" />`
+    })
+  } else {
+    localeCodes.forEach((locale) => {
+      hrefLangs += `<xhtml:link rel="alternate" hreflang="${locale}" href="${wwwDomain}/${locale}${url}" />`
+    })
+  }
+
+  return hrefLangs
+}
+
 // 生成 sitemap
 function generateSitemap(pages) {
-  const date = new Date();
-  const isoString = date.toISOString();
+  const date = new Date()
+  const isoString = date.toISOString()
 
-  let urlTagString = ``;
-  let sitemapContentTemplate = ``;
+  let urlTagString = ``
+  let sitemapContentTemplate = ``
 
   if (IS_PROD) {
     sitemapContentTemplate = `<?xml version="1.0" encoding="utf-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">{{TEMPLATE}}</urlset>`;
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">{{TEMPLATE}}</urlset>`
     pages.forEach((page) => {
-      urlTagString += `<url><loc>${wwwDomain}${page}</loc><changefreq>daily</changefreq><lastmod>${isoString}</lastmod></url>`;
-    });
+      if (page) {
+        const currentPageHrefLangText = addHrefLangToSitemap(page)
+        console.log(`currentPageHrefLangText`, currentPageHrefLangText)
+        console.log(`page`, page)
+        urlTagString += `<url><loc>${wwwDomain}${page}</loc>${currentPageHrefLangText}<changefreq>daily</changefreq><lastmod>${isoString}</lastmod></url>`
+      }
+    })
   } else {
     sitemapContentTemplate = `<?xml version="1.0" encoding="utf-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 {{TEMPLATE}}
-</urlset>`;
+</urlset>`
 
     pages.forEach((page) => {
+      const currentPageHrefLangText = addHrefLangToSitemap(page)
+      console.log(`currentPageHrefLangText`, currentPageHrefLangText)
+      console.log(`page`, page)
       urlTagString += `<url>
   <loc>${wwwDomain}${page}</loc>
+  ${currentPageHrefLangText}
   <changefreq>daily</changefreq>
   <lastmod>${isoString}</lastmod>
 </url>
-`;
-    });
+`
+    })
   }
 
   sitemapContentTemplate = sitemapContentTemplate.replace(
     '{{TEMPLATE}}',
     urlTagString,
-  );
+  )
 
   // 检查文件是否存在
   if (fs.existsSync(sitemapAssetsPath)) {
     // 文件存在，先删除
-    fs.unlinkSync(sitemapAssetsPath);
+    fs.unlinkSync(sitemapAssetsPath)
   }
 
-  fs.writeFileSync(sitemapAssetsPath, sitemapContentTemplate);
+  fs.writeFileSync(sitemapAssetsPath, sitemapContentTemplate)
 }
 
-try {
-  let allPages = [];
-
-  allPages.push(...crawlStaticDirectory(pagesDirectory));
-  allPages.push(
-    ...generateStaticPagesWithLocale(
-      // partners 页面没有 i18n routing, 所以这里需要过滤掉
-      allPages.filter((page) => !page.startsWith('/partners')),
-    ),
-  );
-  allPages.push(...(await generatePromptsPages()));
-  allPages.push(...(await generateToolsPages()));
-  generateSitemap(allPages);
-
-  console.log(`
-  \x1b[32m Sitemap generated successfully! \x1b[0m
-`);
-} catch (error) {
-  console.error(`ERROR:`, error);
+function fixPagesTrailingSlash(pages) {
+  // 不需要 尾斜杠的页面
+  const excludePaths = ['/prompt', '/tools/chatpdf']
+  let responsePages = [...pages]
+  // 如果开启了 trailingSlash，需要将所有页面的末尾添加 /
+  if (nextConfig.trailingSlash) {
+    responsePages = responsePages.map((page) => {
+      if (excludePaths.every((excludePath) => !page.includes(excludePath))) {
+        return page.endsWith('/') ? page : page + '/'
+      }
+      return page
+    })
+  }
+  return responsePages
 }
+
+async function main() {
+  try {
+    // 记录用时
+    const startTime = new Date().getTime()
+    let allPages = []
+
+    allPages.push(...crawlStaticDirectory(pagesDirectory))
+    allPages.push(
+      ...generateStaticPagesWithLocale(
+        // partners 页面没有 i18n routing, 所以这里需要过滤掉
+        allPages.filter((page) => !page.startsWith('/partners')),
+      ),
+    )
+    allPages.push(...(await generateToolsPages()))
+    allPages.push(...(await generatePromptsPages()))
+    allPages.push(...chatpdfUrls)
+
+    generateSitemap(fixPagesTrailingSlash(allPages))
+
+    console.log(`
+  \x1b[32m Sitemap generated successfully! \x1b[0m,
+  \x1b[32m Time taken: ${(new Date().getTime() - startTime) / 1000}s \x1b[0m
+`)
+  } catch (error) {
+    console.error(`ERROR:`, error)
+  }
+}
+main()
