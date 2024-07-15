197d54b0fe0703e40d0725ee863a93aa2af68136
zhouru1bin <109497098+zhouru1bin@users.noreply.github.com>
2024-07-15 20:24:30 +0800

    fix: prompts redirect

--- src/page_components/PromptPages/index.tsx
+++ src/page_components/PromptPages/index.tsx
@@ -1,46 +1,46 @@
-import { Box } from '@mui/material';
-import { useRouter } from 'next/router';
-import { useTranslation } from 'next-i18next';
-import React, { useMemo } from 'react';
+import { Box } from '@mui/material'
+import { useRouter } from 'next/router'
+import { useTranslation } from 'next-i18next'
+import React, { useMemo } from 'react'
 
-import AppDefaultSeoLayout from '@/app_layout/AppDefaultSeoLayout';
-import AppLoadingLayout from '@/app_layout/AppLoadingLayout';
-import useEffectOnce from '@/features/common/hooks/useEffectOnce';
-import { objectToQueryString } from '@/features/common/utils/dataHelper/objectHelper';
+import AppDefaultSeoLayout from '@/app_layout/AppDefaultSeoLayout'
+import AppLoadingLayout from '@/app_layout/AppLoadingLayout'
+import useEffectOnce from '@/features/common/hooks/useEffectOnce'
+import { objectToQueryString } from '@/features/common/utils/dataHelper/objectHelper'
 import {
   PROMPT_LIBRARY_PROXY_BASE_PATH,
   WWW_PROJECT_LINK,
-} from '@/global_constants';
+} from '@/global_constants'
 
 const PromptsPages = () => {
-  const { t } = useTranslation();
-  const router = useRouter();
+  const { t } = useTranslation()
+  const router = useRouter()
 
   const canonicalLink = useMemo(() => {
-    const locale = router.query.locale;
-    let fullHref = `${WWW_PROJECT_LINK}${PROMPT_LIBRARY_PROXY_BASE_PATH}`;
+    const locale = router.query.locale
+    let fullHref = `${WWW_PROJECT_LINK}${PROMPT_LIBRARY_PROXY_BASE_PATH}`
     if (locale) {
-      fullHref = `${fullHref}${locale ? `/${locale}` : ''}`;
+      fullHref = `${fullHref}${locale ? `/${locale}` : ''}`
     }
 
-    return `${fullHref}/library`;
-  }, [router.query.locale]);
+    return `${fullHref}/library`
+  }, [router.query.locale])
 
   useEffectOnce(() => {
-    const cloneQuery = { ...router.query };
-    let prefixHref = PROMPT_LIBRARY_PROXY_BASE_PATH;
+    const cloneQuery = { ...router.query }
+    let prefixHref = PROMPT_LIBRARY_PROXY_BASE_PATH
     if (cloneQuery.locale) {
-      prefixHref += `/${cloneQuery.locale}`;
-      delete cloneQuery.locale;
+      prefixHref += `/${cloneQuery.locale}`
+      delete cloneQuery.locale
     }
 
-    let queryString = '';
+    let queryString = ''
     if (Object.keys(cloneQuery).length > 0) {
-      queryString = '?' + objectToQueryString(cloneQuery);
+      queryString = '?' + objectToQueryString(cloneQuery)
     }
 
-    router.push(`${prefixHref}/library${queryString}`);
-  });
+    location.replace(`${WWW_PROJECT_LINK}${prefixHref}/library${queryString}`)
+  })
 
   return (
     <Box height={'50vh'}>
@@ -53,7 +53,7 @@ const PromptsPages = () => {
 
       <AppLoadingLayout loading />
     </Box>
-  );
-};
+  )
+}
 
-export default PromptsPages;
+export default PromptsPages
