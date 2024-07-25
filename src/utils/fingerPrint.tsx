ebe63c05b91c2adcf0867114aaef3513cafbbae3
zhouru1bin <109497098+zhouru1bin@users.noreply.github.com>
2024-07-25 20:10:45 +0800

    opt: remove fingerprintjs

--- src/utils/fingerPrint.tsx
+++ src/utils/fingerPrint.tsx
@@ -1,28 +1,29 @@
-import FingerprintJS from '@fingerprintjs/fingerprintjs'
+// import FingerprintJS from '@fingerprintjs/fingerprintjs'
 
-import { getLocalStorage, setLocalStorage } from '@/utils/localStorage'
+import { getLocalStorage } from '@/utils/localStorage'
 
 export const FINGER_PRINT_LOCAL_STORAGE_SAVE_KEY = 'x_fid'
 
 export const initFingerPrint = (): Promise<string | undefined> => {
   return new Promise((resolve) => {
-    if (typeof window !== 'undefined') {
-      try {
-        const fpPromise = FingerprintJS.load()
-        fpPromise.then((fp) => {
-          fp.get().then((result) => {
-            setLocalStorage(
-              FINGER_PRINT_LOCAL_STORAGE_SAVE_KEY,
-              result.visitorId,
-            )
-            resolve(result.visitorId)
-          })
-        })
-      } catch (e) {
-        console.log('initFingerPrint error: \t', e)
-        resolve(undefined)
-      }
-    }
+    resolve('')
+    // if (typeof window !== 'undefined') {
+    //   try {
+    //     const fpPromise = FingerprintJS.load()
+    //     fpPromise.then((fp) => {
+    //       fp.get().then((result) => {
+    //         setLocalStorage(
+    //           FINGER_PRINT_LOCAL_STORAGE_SAVE_KEY,
+    //           result.visitorId,
+    //         )
+    //         resolve(result.visitorId)
+    //       })
+    //     })
+    //   } catch (e) {
+    //     console.log('initFingerPrint error: \t', e)
+    //     resolve(undefined)
+    //   }
+    // }
   })
 }
 export const getFingerPrint = () => {
