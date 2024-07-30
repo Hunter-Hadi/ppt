32a8899e9439f5ab48fc96011bddd5e5cf99a5d8
laizeping <laizeping@simplylab.xyz>
2024-07-30 11:54:53 +0800

    feat(i18n): change description

--- update_i18n.mjs
+++ update_i18n.mjs
@@ -1,6 +1,6 @@
 import { buildI18n } from './scripts/build-i18n-script.mjs'
 
 const forceUpdateKeys = []
-const retryLanguageCodes = ['th', 'el', 'hi']
+const retryLanguageCodes = []
 
 buildI18n(forceUpdateKeys, retryLanguageCodes).then().catch()
