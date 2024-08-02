174d431a7dfa0e132aa5c4e56fd5a2aefbd5b361
zhouru1bin <109497098+zhouru1bin@users.noreply.github.com>
2024-08-02 15:52:56 +0800

    chore: remove prompt features files

--- src/features/prompt/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { LanguageSelect } from './LanguageSelect';
-export { PromptCard, PromptCardTag } from './PromptCard';
-export { PromptTagSelector } from './PromptTagSelector';
-export { ToneSelect } from './ToneSelect';
-export { WritingStyleSelect } from './WritingStyleSelect';
--- src/features/prompt/layout/index.ts
+++
@@ -1,1 +0,0 @@
-export { PromptListLayout } from './PromptListLayout';
