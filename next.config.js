6d23e7964b009e9fb60cc5da3f42230d6fa0ae65
zhouru1bin <109497098+zhouru1bin@users.noreply.github.com>
2024-08-05 15:42:44 +0800

    fix: nextjs config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 // const { i18n } = require('./next-i18next.config');
 
-const WebpackObfuscatorPlugin = require('webpack-obfuscator')
 /** @type {import('next').NextConfig} */
 
-
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
