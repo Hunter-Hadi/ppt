47fab3b52b67b39a0150d73b39438bccc7f0ff35
zhouru1bin <109497098+zhouru1bin@users.noreply.github.com>
2024-08-08 21:18:50 +0800

    chore: landing ab test v9

--- src/features/ab_tester/constant/landingVariant.ts
+++ src/features/ab_tester/constant/landingVariant.ts
@@ -1,10 +1,16 @@
 export type ILandingVariantType =
-  | 'cloudflare_video' // cloudflare video
-  | 'youtube_video' // youtube video
+  | 'title1'
+  | 'title2'
+  | 'title3'
+  | 'title4'
+  | 'title5'
 
 export const LANDING_VARIANTS = [
-  'cloudflare_video',
-  'youtube_video',
+  'title1',
+  'title2',
+  'title3',
+  'title4',
+  'title5',
 ] as ILandingVariantType[]
 
 export const TESTER_LANDING_PATH_TARGET_PATHNAME = [
@@ -12,15 +18,17 @@ export const TESTER_LANDING_PATH_TARGET_PATHNAME = [
   '/partners/updated',
   '/partners/installed',
   '/partners/uninstalled',
-  (pathname: string) => pathname.startsWith('/features'),
 ] //开启路径，进入页面触发abtest进入通知，和语言跳转
 
-export const TEST_LANDING_COOKIE_NAME = 'maxai-lpv-v8' //abtest的版本名称
+export const TEST_LANDING_COOKIE_NAME = 'maxai-lpv-v9' //abtest的版本名称
 
 export const LANDING_VARIANT_TO_VERSION_MAP: Record<
   ILandingVariantType,
   string
 > = {
-  cloudflare_video: '8-1',
-  youtube_video: '8-2',
+  title1: '9-1',
+  title2: '9-2',
+  title3: '9-3',
+  title4: '9-4',
+  title5: '9-5',
 } //abtest的版本对应的后端版本号,通知后端，以及开发时选择的版本号
--- src/page_components/FeaturesLandingPages/components/FeaturesExploreMore.tsx
+++ src/page_components/FeaturesLandingPages/components/FeaturesExploreMore.tsx
@@ -7,7 +7,6 @@ import { useRouter } from 'next/router'
 import { useTranslation } from 'next-i18next'
 import React, { FC, useEffect } from 'react'
 
-import useLandingABTester from '@/features/ab_tester/hooks/useLandingABTester'
 import CallToActionSection from '@/features/landing/components/CallToActionSection'
 import FeaturesContentAbTestV7AutoVideo from '@/features/landing/components/FeaturesCarousel/FeaturesContentAbTestV7SlideAutoVideo'
 import HeroSection from '@/features/landing/components/HeroSection'
@@ -23,8 +22,6 @@ interface IProps {
 const FeaturesExploreMore: FC<IProps> = ({ propRef, sx }) => {
   const { t } = useTranslation()
   const { isReady, asPath } = useRouter()
-
-  useLandingABTester(true)
 
   useEffect(() => {
     if (isReady && asPath) {
@@ -82,7 +79,6 @@ const FeaturesExploreMore: FC<IProps> = ({ propRef, sx }) => {
         showIndicatorBadge={false}
         propRef={propRef}
         // loading={!loaded}
-        inLandingVideoABTest
         heroVideoProps={{
           videoSrc: LANDING_PRIMARY_VIDEO_ASSETS_URL,
           videoPosterUrl: `/assets/landing/hero-section/video-cover.png`,
